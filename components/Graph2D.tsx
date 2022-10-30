1e367dffd9c10ed7ea7bb0775ad685e0af5e2d61
Jongbeom Lee <devleejb@gmail.com>
2022-10-30 21:30:39 +0900

    react-force-graph 추가

---
+++ components/Graph2D.tsx
@@ -0,0 +1,8 @@
+import { FunctionComponent } from "react";
+import { ForceGraph2D } from "react-force-graph";
+
+const Graph2D: typeof ForceGraph2D = (props) => {
+  return <ForceGraph2D {...props} />;
+};
+
+export default Graph2D;

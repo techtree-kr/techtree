3ed60fa86b66d729eba931b86e7d25d9576fd5cc
Jongbeom Lee <devleejb@gmail.com>
2022-10-30 21:42:38 +0900

    불필요 import 삭제

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from "next";
 import dynamic from "next/dynamic";
 import styles from "../styles/Home.module.css";
-import { Button } from "@mui/material";
 
 const Graph2D = dynamic(() => import("../components/Graph2D"), {
   ssr: false,

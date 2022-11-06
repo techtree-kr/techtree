ffcae8e1aa6ac535530f331a3ecbe560588c34e0
kuk <kuk@codev.kr>
2022-11-07 01:10:04 +0900

    add nodejs md
    add nodejs md

--- pages/contents/[directory]/[markdown].tsx
+++ pages/contents/[directory]/[markdown].tsx
@@ -1,5 +1,16 @@
+import { useRouter } from "next/router";
+// import fs from "fs";
+
 const MarkdownPage = () => {
-  return <div></div>;
+  const router = useRouter();
+  // const readme = fs.readFileSync("README.md", "utf-8");
+
+  return (
+    <div>
+      <div>{router.asPath}</div>
+      {/* <div>{readme}</div> */}
+    </div>
+  );
 };
 
 export default MarkdownPage;
